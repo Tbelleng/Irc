c6ce046df89738f81d00a5632fbb14da8ff79940
0xTrinityy <trinity.0x@protonmail.com>
2023-10-06 15:25:06 +0200

    Starting the project

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelleng <tbelleng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:19:46 by tbelleng          #+#    #+#             */
+/*   Updated: 2023/10/05 21:38:47 by tbelleng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "server.hpp"
+
+int main(int argc, char **argv) 
+{
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) 
+    {
+        std::cerr << "Can't create a socket!" << std::endl;
+        return -1;
+    }
+	
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    char buffer[] = "testing IRC";
+    send(client_socket, buffer, sizeof(buffer), 0);
+
+    close(client_socket);
+
+    return 0;
+}
---
+++ server.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelleng <tbelleng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 17:59:39 by tbelleng          #+#    #+#             */
+/*   Updated: 2023/10/05 21:54:42 by tbelleng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+# include <iostream>
+# include <cstring>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <sys/epoll.h>
+# include <arpa/inet.h>
+
+
+
+#endif
