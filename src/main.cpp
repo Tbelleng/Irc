ec29c352b6132ec092f0696e75bd76fe054fe582
lucienfer <lucien.fernandez02@gmail.com>
2023-10-06 17:13:58 +0200

    rien

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,5 @@
 
 int main(int argc, char **argv) 
 {
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) 
-    {
-        std::cerr << "Can't create a socket!" << std::endl;
-        return -1;
-    }
-	
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
-
-    connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-    char buffer[] = "testing IRC";
-    send(client_socket, buffer, sizeof(buffer), 0);
-
-    close(client_socket);
-
-    return 0;
+ 
 }
