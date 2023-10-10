9e6572b8cc7980c24a0c9974e1bd17dc1269870f
0xTrinityy <trinity.0x@protonmail.com>
2023-10-10 21:30:05 +0200

    Use listen/CTL to add every new client to the socket

--- src/epoll_handle.cpp
+++ src/epoll_handle.cpp
@@ -6,32 +6,32 @@
 /*   By: tbelleng <tbelleng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 16:27:06 by tbelleng          #+#    #+#             */
-/*   Updated: 2023/10/08 16:36:13 by tbelleng         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:07:23 by tbelleng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../server.hpp"
 
-epoll_event    new_epoll(int serverSocket)
-{
-	int epoll_fd = epoll_create1(0);
-	if (epoll_fd == -1)
-	{
-        perror("Error while creating Epoll");
-        return ;
-	}
+// epoll_event    new_epoll(int serverSocket)
+// {
+// 	int epoll_fd = epoll_create1(0);
+// 	if (epoll_fd == -1)
+// 	{
+//         perror("Error while creating Epoll");
+//         return ;
+// 	}
         
-    struct epoll_event event;
-	event.data.fd = serverSocket;
-	event.events = EPOLLIN;
+//     struct epoll_event event;
+// 	event.data.fd = serverSocket;
+// 	event.events = EPOLLIN;
 
-	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, serverSocket, &event) == -1) 
-	{
-		perror("Error adding socket");
-        close(epoll_fd);
-        close(serverSocket);
-        return ;
-	}
-	return (event);
-}
+// 	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, serverSocket, &event) == -1) 
+// 	{
+// 		perror("Error adding socket");
+//         close(epoll_fd);
+//         close(serverSocket);
+//         return ;
+// 	}
+// 	return (event);
+// }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tbelleng <tbelleng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:19:46 by tbelleng          #+#    #+#             */
-/*   Updated: 2023/10/08 16:29:01 by tbelleng         ###   ########.fr       */
+/*   Updated: 2023/10/10 20:02:25 by tbelleng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@
 #include <sys/socket.h>
 #include <map>
 
+int MAX_UTILISATEURS = 5;
+
 int main(int argc, char **argv) 
 {
     (void)argc;
     (void)argv;
+    
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "Error creating socket" << std::endl;
@@ -39,50 +42,110 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (listen(serverSocket, 5) == -1) {
+    int epoll_fd = epoll_create1(0);
+	if (epoll_fd == -1)
+	{
+        perror("Error while creating Epoll");
+        return 0;
+	}
+        
+    struct epoll_event event;
+	event.data.fd = serverSocket;
+	event.events = EPOLLIN;
+
+	
+    if (listen(serverSocket, MAX_UTILISATEURS) == -1) 
+    {
         std::cerr << "Error listening on socket" << std::endl;
         close(serverSocket);
         return 1;
     }
 
     std::cout << "Server listening on port 5500..." << std::endl;
-
-    while (true) {
-        struct sockaddr_in clientAddress;
-        socklen_t clientAddrLen = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrLen);
-
-        if (clientSocket == -1) {
-            std::cerr << "Error accepting connection" << std::endl;
-            continue;
+	
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, serverSocket, &event) == -1) 
+	{
+		perror("Error adding socket");
+        close(epoll_fd);
+        close(serverSocket);
+        return 0;
+	}
+ 
+    while (true) 
+    {
+        struct epoll_event events[1];
+        int new_event = epoll_wait(epoll_fd, events, MAX_UTILISATEURS, -1);
+        if (new_event == -1) {
+            perror("epoll_wait");
+            return 0;
         }
+        int i = 0;
+        while (i < new_event)
+        {
+            if (events[i].data.fd == serverSocket)
+            {
+                struct sockaddr_in clientAddress;
+                socklen_t clientAddrLen = sizeof(clientAddress);
+                int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrLen);
+            
+                if (clientSocket == -1)
+                    perror("accept");
+                else
+                {
+                    event.events = EPOLLIN;
+                    event.data.fd = clientSocket;
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clientSocket, &event) == -1)
+                    {
+                        perror("epoll_ctl");
+                        return 0;
+                    }
+                }
+            }
+            else 
+            {
+                char buffer[512];
+                int bytes_read = read(events[i].data.fd, buffer, sizeof(buffer));
+                write(events[i].data.fd, buffer, bytes_read);
+            }
+            i++;
+        }
+        // if (clientSocket == -1) 
+        // {
+        //     std::cerr << "Error accepting connection" << std::endl;
+        //     continue;
+        // }
         
-        char buffer[256];
-        ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead == -1) {
-            std::cerr << "Error receiving data" << std::endl;
-            close(clientSocket);
-            continue;
-        }
+        // char buffer[256];
+        // ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+        // if (bytesRead == -1) 
+        // {
+        //     std::cerr << "Error receiving data" << std::endl;
+        //     close(clientSocket);
+        //     continue;
+        // }
 
-        buffer[bytesRead] = '\0';
+        // buffer[bytesRead] = '\0';
 
-        std::cout << "On recoit le message : " << buffer << std::endl;
-        std::cout << "Received message: " << buffer << std::endl;
+        // std::cout << "On recoit le message : " << buffer << std::endl;
+        // std::cout << "Received message: " << buffer << std::endl;
     
-        if (strncmp(buffer, "CAP LS", 6) == 0) {
-        const char* capListResponse = "/USER\n";
-        ssize_t capListBytesSent = send(clientSocket, capListResponse, strlen(capListResponse), 0);
+        // if (strncmp(buffer, "CAP LS", 6) == 0) 
+        // {
+        // const char* capListResponse = "/USER\n";
+        // ssize_t capListBytesSent = send(clientSocket, capListResponse, strlen(capListResponse), 0);
         
-        (void)capListBytesSent;
-        }
-        const char* response = "Message received\n";
-        ssize_t bytesSent = send(clientSocket, response, strlen(response), 0);
-        if (bytesSent == -1) {
-            std::cerr << "Error sending response" << std::endl;
-        }
+        // (void)capListBytesSent;
+        // }
+        // const char* response = "Message received\n";
+        // ssize_t bytesSent = send(clientSocket, response, strlen(response), 0);
+        // if (bytesSent == -1) 
+        // {
+        //     std::cerr << "Error sending response" << std::endl;
+        // }
         //if (false)
         //    close(clientSocket);
+        std::cout << "SERVER EPOLL :" << events[0].data.fd << std::endl;
+        std::cout << "SERVER EPOLL :" << events[1].data.fd << std::endl;
     }
     close(serverSocket);
     return 0;
